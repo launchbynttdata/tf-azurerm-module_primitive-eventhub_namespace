e55bf4c20217fc84ca123e9582aebc44158eb592
Chandrabhanlanguriya-Ram-nttd <349712@nttdata.com>
2026-06-10 18:41:41 +0530

    feat: add network rule set support and v4 provider compatibility (#14)
    Co-authored-by: Chandrabhanlanguriya-Ram-nttd <Chandrabhanlanguriya.Ram@nttdata.com>

--- main.tf
+++ main.tf
@@ -19,5 +19,44 @@ resource "azurerm_eventhub_namespace" "eventhub_namespace" {
   capacity                      = var.capacity
   public_network_access_enabled = var.public_network_access_enabled
 
+  dynamic "network_rulesets" {
+    for_each = var.network_rule_set != null ? [var.network_rule_set] : []
+
+    content {
+      default_action                 = network_rulesets.value.default_action
+      public_network_access_enabled  = var.public_network_access_enabled
+      trusted_service_access_enabled = network_rulesets.value.trusted_service_access_enabled
+
+      dynamic "ip_rule" {
+        for_each = network_rulesets.value.ip_rules
+
+        content {
+          ip_mask = ip_rule.value.ip_mask
+          action  = ip_rule.value.action
+        }
+      }
+
+      dynamic "virtual_network_rule" {
+        for_each = network_rulesets.value.virtual_network_rules
+
+        content {
+          subnet_id                                       = virtual_network_rule.value.subnet_id
+          ignore_missing_virtual_network_service_endpoint = virtual_network_rule.value.ignore_missing_virtual_network_service_endpoint
+        }
+      }
+    }
+  }
+
   tags = var.tags
+
+  lifecycle {
+    precondition {
+      condition = (
+        (var.sku == "Basic" && var.capacity == 1) ||
+        (var.sku == "Standard" && var.capacity >= 1 && var.capacity <= 20) ||
+        (var.sku == "Premium" && var.capacity >= 1 && var.capacity <= 4)
+      )
+      error_message = "The capacity must be 1 for Basic, between 1-20 for Standard, or between 1-4 for Premium."
+    }
+  }
 }
